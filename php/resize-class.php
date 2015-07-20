86b90bd100019cb6ae9ea54edd24de8932ddf118
Troy Huang <troycaeser@dyn-118-139-4-62.its.monash.edu.au>
2015-07-20 17:11:30 +1000

    watermark now possible

--- php/resize-class.php
+++ php/resize-class.php
@@ -202,6 +202,28 @@
 			}
 
 			## --------------------------------------------------------
+			
+			public function restamp($stamps, $photo){
+				// Load the stamp and the photo to apply the watermark to
+				$stamp = imagecreatefromjpeg($stamps);
+				$im = imagecreatefromjpeg($photo);
+
+				// Set the margins for the stamp and get the height/width of the stamp image
+				$marge_right = 10;
+				$marge_bottom = 10;
+				$sx = imagesx($stamp);
+				$sy = imagesy($stamp);
+
+				// Copy the stamp image onto our photo using the margin offsets and the photo 
+				// width to calculate positioning of the stamp. 
+				imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+
+				// Output and free memory
+				imagejpeg($im, $photo, 100);
+				imagedestroy($im);
+			}
+
+			## --------------------------------------------------------
 
 			public function saveImage($savePath, $imageQuality="100")
 			{
--- php/resize.php
+++ php/resize.php
@@ -1,5 +1,5 @@
 <?php
-	include("resize-class.php");
+	include 'resize-class.php';
 
 	function resize_img($path, $name, $dir){
 		
@@ -11,6 +11,7 @@
 		$resizeObj -> resizeImage(400, 300, 'exact');
 
 		$resizeObj -> saveImage('../temp/'.$dir."/".$name.'.jpg', 100);
-	}
 
+		$resizeObj -> restamp('../bin/chisholm_gamon/logo.jpeg', '../temp/'.$dir."/".$name.'.jpg');
+	}
 ?>

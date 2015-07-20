f5a5ee0f8eae35342b1611f38f755eb8b68c6031
godfrey22 <g710110437@gmail.com>
2015-07-20 14:29:30 +0800

    fold images and download

--- php/download.php
+++ php/download.php
@@ -1,11 +1,11 @@
 <?php
 
-	function download_img($url, $name){
+	function download_img($url, $dir, $name){
 		//get the file
 		$content = file_get_contents($url);
 
 		//save locally
-		$fp = fopen("../temp/".$name.".jpg", 'wb');
+		$fp = fopen("../temp/".$dir."/".$name.".jpg", 'wb');
 		fwrite($fp, $content);
 		fclose($fp);
 	}
